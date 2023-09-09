04d43ace95a4d73c143c33c608cec0d9d08ac198
TeerawatTan <teerawat.tanatan@gmail.com>
2023-09-09 23:24:37 +0700

    Add test controller

---
+++ HelpCheck_API/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HelpCheck_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> TestApi()
+        {
+            return Ok("Connected.");
+        }
+    }
+}
